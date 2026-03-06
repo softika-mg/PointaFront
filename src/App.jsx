67e0cb910c5fc5025cc77b8c580b839e6f44b302
MeganeTech <omegarabemananjara3@gmail.com>
2026-03-06 08:38:51 -0500

    Deuxieme version

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,12 @@
 import { useState } from 'react'
-import Sidebar from './components/Sidebar'
 import MainContent from './components/MainContent'
-import Entete from './components/header'
+
 
 function App() {
   const [active, setActive] = useState('taches')
 
   return (
     <div className="w-full mx-auto h-screen overflow-hidden">
-      {/* <Entete active={active} setActive={setActive} />
-      <Sidebar active={active} 
-      setActive={setActive}
-      tacheCount={3}/> */}
       <MainContent active={active} setActive={setActive}/>
     </div>
   )
